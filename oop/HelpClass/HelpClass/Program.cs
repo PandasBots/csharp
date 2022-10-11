5279a49067b3a3cc5b07a5d07a941fde294076ee
PandasBots <comercial@pandasbots.com>
2022-10-11 19:37:33 -0300

    update

---
+++ oop/HelpClass/HelpClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelpClass // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Instancia objeto da classe triangulo
+            Triangulo x;
+            x = new Triangulo();
+            x.A = 3;
+            x.B = 4;
+            x.C = 5;
+            Console.WriteLine(x.A);
+            //
+            double areaX = x.Area();
+            Console.WriteLine(areaX);
+        }
+    }
+}
