0997f5b368a0c70764f5a2c066445d6044b8d28b
PandasBots <comercial@pandasbots.com>
2022-10-19 11:02:41 -0300

    created - camada jogo xadrez

---
+++ _apps-pilot/xadrez/xadrez_console/xadrez_console/xadrez/Torre.cs
@@ -0,0 +1,19 @@
+﻿using tabuleiro;
+namespace xadrez
+{
+    class Rei : Peca
+    {
+        // Construtor
+        // Ao receber um tabuleiro e uma cor, ele repassa para o método da classe base: Peca.
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+        }
+
+        // Métodos
+        public override string ToString()
+        {
+            return "K";
+        }
+    }
+}
+
