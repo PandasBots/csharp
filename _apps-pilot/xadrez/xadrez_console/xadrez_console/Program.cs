cf08ea7e9aa5f33814cec98de67bbaf46e93cf51
PandasBots <comercial@pandasbots.com>
2022-10-19 09:44:39 -0300

    created - class Posicao

---
+++ _apps-pilot/xadrez/xadrez_console/xadrez_console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Variável
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine($"Posição: {P}");
+        }
+    }
+}
---
+++ _apps-pilot/xadrez/xadrez_console/xadrez_console/Tabuleiro/Posicao.cs
@@ -0,0 +1,24 @@
+﻿namespace Tabuleiro
+{
+    class Posicao
+    {
+        // 1. Atributos
+        public int linha { get; set; }
+        public int coluna { get; set; }
+        // 2. Construtores
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+
+        }
+        // 3. Métodos
+        // Override o método ToString da Classe Object.
+        // Mensagem que será impressa toda vez que mandarmos imprimir o objeto instanciado por esta classe.
+        public override string ToString()
+        {
+            return $"{linha}, {coluna}";
+        }
+    }
+}
+
